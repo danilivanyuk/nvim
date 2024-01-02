1622221161838a17c6e32da960eb7d7a0e818006
danilivanyuk <danilivanyuk00@gmail.com>
2024-01-02 19:35:57 +0600

    settings for telescope lsp treesitter

--- init.lua
+++ init.lua
@@ -1,25 +1,7 @@
-require("mappings")
-require("plugins")
-vim.loader.enable()
-local core_conf_files = {
-  "globals.lua", -- some global settings
---"options.vim", -- setting options in nvim
-  -- "autocommands.vim", -- various autocommands
-  "mappings.lua", -- all the user-defined mappings
-  -- "plugins.vim", -- all the plugins installed and their configurations
-  --"colorschemes.lua", -- colorscheme settings
-}
+require('globals')
+require('mappings')
+require('plugins')
 
-local viml_conf_dir = vim.fn.stdpath("config") .. "/viml_conf"
--- source all the core config files
-for _, file_name in ipairs(core_conf_files) do
-  if vim.endswith(file_name, 'vim') then
-    local path = string.format("%s/%s", viml_conf_dir, file_name)
-    local source_cmd = "source " .. path
-    vim.cmd(source_cmd)
-  else
-    local module_name, _ = string.gsub(file_name, "%.lua", "")
-    package.loaded[module_name] = nil
-    require(module_name)
-  end
-end
+require('neodev').setup()
+
+-- vim: ts=2 sts=2 sw=2 et
--- lua/globals.lua
+++ lua/globals.lua
@@ -1,21 +1,15 @@
-
-vim.opt.nu = true
-vim.opt.relativenumber = true
-
--- OS
-vim.o.clipboard = 'unnamedplus'
-vim.o.mouse = 'a'
-
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 vim.o.hlsearch = false
 vim.wo.number = true
+vim.o.mouse = 'a'
+vim.o.clipboard = 'unnamedplus'
 vim.o.breakindent = true
-vim.wo.signcolumn = 'yes'
-vim.o.completeopt = 'menuone,noselect'
-
--- Search
+vim.o.undofile = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
-
--- Update time
+vim.wo.signcolumn = 'yes'
 vim.o.updatetime = 250
 vim.o.timeoutlen = 300
+vim.o.completeopt = 'menuone,noselect'
+vim.o.termguicolors = true
